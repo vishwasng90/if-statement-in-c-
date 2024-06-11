21eb547308f742c2bd6160eab681c4c128362815
vishwasng90 <164770369+vishwasng90@users.noreply.github.com>
2024-06-11 11:05:30 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 20;
+            int y = 18;
+            if (x > y)
+            {
+                Console.WriteLine("x is greter than y");
+                Console.ReadLine();
+            }
+        }
+    }
+}
